daafb4ef89a494ee97ce645218934c98b2d6b0f6
Ray Zana <rayzana280@gmail.com>
2023-09-04 22:08:39 -0400

    commit tree 7d6191504adcb2b612b815823d2c9bbf4bec0c60

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    #setter
+    def name=(name)
+        @name = name 
+    end 
+    #getter
+    def name
+        @name 
+    end 
 
+    def breed=(dog_breed)
+        @breed = dog_breed 
+    end 
+
+    def breed 
+        @breed
+    end 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+    #setter
+    def name=(person_name)
+        @name = person_name 
+    end 
+    #getter
+    def name 
+        @name 
+    end 
 
+    def job=(person_job)
+        @job = person_job 
+    end 
+
+    def job 
+        @job
+    end 
 end
